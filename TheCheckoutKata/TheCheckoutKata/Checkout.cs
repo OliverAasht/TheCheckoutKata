9332bc80a28b774dec36480992778e6c7cd4ca01
OliverAasht-INV <oliver.aasht@inventry.co.uk>
2024-08-18 11:30:38 +0100

    impl GetTotalPrice(), test now passing

--- TheCheckoutKata/TheCheckoutKata/Checkout.cs
+++ TheCheckoutKata/TheCheckoutKata/Checkout.cs
@@ -6,7 +6,28 @@
 
         public int GetTotalPrice()
         {
-            throw new NotImplementedException();
+            int totalPrice = 0;
+
+            foreach (var item in items)
+            {
+                var rule = pricingRules.GetRule(item.Key);
+                if (rule == null)
+                    continue;
+
+                if (rule.SpecialQuantity.HasValue && item.Value >= rule.SpecialQuantity.Value)
+                {
+                    int specialSets = item.Value / rule.SpecialQuantity.Value;
+                    int remainder = item.Value % rule.SpecialQuantity.Value;
+
+                    totalPrice += specialSets * rule.SpecialPrice.Value;
+                    totalPrice += remainder * rule.UnitPrice;
+                    continue;
+                }
+
+                totalPrice += item.Value * rule.UnitPrice;
+            }
+
+            return totalPrice;
         }
 
         public void Scan(string item)
